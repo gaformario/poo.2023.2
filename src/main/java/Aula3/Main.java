a403dfef0a0e41b60f929858e47422d9a23f63af
gaformario <gabriel.formario@hotmail.com>
2023-08-17 13:03:58 -0300

    att

---
+++ src/main/java/Aula3/Main.java
@@ -0,0 +1,17 @@
+package Aula3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Instanciar objeto do tipo Conta
+        Conta contaHumberto = new Conta();
+        
+        contaHumberto.depositar(9.45);
+        contaHumberto.sacar(20);
+        contaHumberto.sacar(800);
+
+        System.out.println(contaHumberto.getSaldo());
+
+    }
+
+}
